f633b5cacc268fb6894e570db9d22a13be042ec4
Carlos Joel Tavares da Silva <carlosjoel.tavares@gmail.com>
2015-07-02 15:33:06 -0300

    Buscar pelo nome

--- application/views/favorecido/resultado.php
+++ application/views/favorecido/resultado.php
@@ -1,5 +1,19 @@
 <?php $this->load->view('_inc/header')?>
 
-<?php var_dump($favorecidos) ?>
+<table class="table">
+    <tr>
+        <th>NIS</th>
+        <th>Nome</th>
+        <th>Município</th>
+    </tr>
+    <?php foreach ($favorecidos as $favorecido): ?>
+            <tr>
+                <td><?php echo(get_object_vars ($favorecido)['NIS Favorecido'])?></td>
+                <td><?php echo(get_object_vars ($favorecido)['NomeFavorecido'])?></td>
+                <td><?php echo(get_object_vars ($favorecido)['Código SIAFI Município'])?></td>
+            </tr>
+    <?php endforeach ?>
+    <hr />
+</table>
 
 <?php $this->load->view('_inc/footer') ?>
